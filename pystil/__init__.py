06617911bfad0586af516e4624b2deb37a08eb75
Florian Mounier <florian.mounier@kozea.fr>
2011-08-09 12:04:28 +0200

    Add License

--- pystil/__init__.py
+++ pystil/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# Copyright (C) 2011 by Florian Mounier, Kozea
+# This file is part of pystil, licensed under a 3-clause BSD license.
 """
 pystil - An elegant visit tracker
 """
