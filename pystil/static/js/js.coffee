78f8aa09e48e0e57e614012f346e175be85cdfff
Florian Mounier <florian.mounier@kozea.fr>
2011-08-10 18:57:57 +0200

    Coffeeing draw script

---
+++ pystil/static/js/js.coffee
@@ -0,0 +1,35 @@
+draw = () ->
+    for g in window.graphs
+            $("#graphs").append $('<div>').attr('id', g.name).addClass("graph " + g.classname)
+            elt = $("#" + g.name)
+
+            plot = $.plot(elt, [], g.options);
+
+            # Keep the closures FFS
+            success = ((g, elt) ->
+                (response) ->
+                    plot = $.plot(elt, g.data(response), g.options))(g, elt)
+            $.ajax
+                url: g.url
+                method: 'GET'
+                dataType: 'json'
+                success: success
+
+            previousPoint = null
+            elt.bind("plothover", ((g) -> (event, pos, item) ->
+                if item
+                    index = if item.series.pie.show then item.seriesIndex else item.dataIndex
+                    if previousPoint != index
+                        previousPoint = index
+                        $("#tooltip").remove()
+                        $('<div>').attr('id', 'tooltip').css
+                            top: pos.pageY + 5,
+                            left: pos.pageX + 5,
+                            border: '1px solid ' + item.series.color
+                        .text(g.tooltip item).appendTo("body")
+                    else
+                        $("#tooltip").remove()
+                        previousPoint = null
+            )(g))
+
+setTimeout draw, 10
--- pystil/static/js/js.js
+++ pystil/static/js/js.js
@@ -1,44 +1,48 @@
-$(function () {
-    for(var i in graphs) {
-        var g = graphs[i];
-        $("#graphs").append(
-            $('<div>').attr('id', g.name).addClass("graph " + g.classname));
-        var elt = $("#" + g.name);
-        var opt = g.options;
-        var plot = $.plot(elt, [], g.options);
-        // Keep the closures FFS
-        var success = function (g, elt) {
-            return function (response) {
-                plot = $.plot(elt, g.data(response), g.options);
-            };
-        }(g, elt);
-        $.ajax({
-            url: g.url,
-            method: 'GET',
-            dataType: 'json',
-            success: success
-        });
-
-        var previousPoint = null;
-        elt.bind("plothover", function (g) {
-            return function (event, pos, item) {
-                if (item) {
-                    var index = item.series.pie.show ? item.seriesIndex : item.dataIndex;
-                    if (previousPoint != index) {
-                        previousPoint = index;
-                        $("#tooltip").remove();
-                        $('<div>').attr('id', 'tooltip').css({
-                            top: pos.pageY + 5,
-                            left: pos.pageX + 5,
-                            border: '1px solid ' + item.series.color
-                        }).text(g.tooltip(item)).appendTo("body");
-                    }
-                }
-                else {
-                    $("#tooltip").remove();
-                    previousPoint = null;
-                }
-            };
-        }(g));
+(function() {
+  var draw;
+  draw = function() {
+    var elt, g, plot, previousPoint, success, _i, _len, _ref, _results;
+    _ref = window.graphs;
+    _results = [];
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      g = _ref[_i];
+      $("#graphs").append($('<div>').attr('id', g.name).addClass("graph " + g.classname));
+      elt = $("#" + g.name);
+      plot = $.plot(elt, [], g.options);
+      success = (function(g, elt) {
+        return function(response) {
+          return plot = $.plot(elt, g.data(response), g.options);
+        };
+      })(g, elt);
+      $.ajax({
+        url: g.url,
+        method: 'GET',
+        dataType: 'json',
+        success: success
+      });
+      previousPoint = null;
+      _results.push(elt.bind("plothover", (function(g) {
+        return function(event, pos, item) {
+          var index;
+          if (item) {
+            index = item.series.pie.show ? item.seriesIndex : item.dataIndex;
+            if (previousPoint !== index) {
+              previousPoint = index;
+              $("#tooltip").remove();
+              return $('<div>').attr('id', 'tooltip').css({
+                top: pos.pageY + 5,
+                left: pos.pageX + 5,
+                border: '1px solid ' + item.series.color
+              }).text(g.tooltip(item)).appendTo("body");
+            } else {
+              $("#tooltip").remove();
+              return previousPoint = null;
+            }
+          }
+        };
+      })(g)));
     }
-});
+    return _results;
+  };
+  setTimeout(draw, 10);
+}).call(this);
