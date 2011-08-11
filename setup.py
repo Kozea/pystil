f511d7d67efa24639729f25255f5c98fbcee677b
Florian Mounier <florian.mounier@kozea.fr>
2011-08-11 11:07:10 +0200

    Add a qnd setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2011 by Florian Mounier, Kozea
+# This file is part of pystil, licensed under a 3-clause BSD license.
+
+"""
+pystil - An elegant site web traffic analyzer
+"""
+
+from setuptools import setup, find_packages
+
+# Use a time-based version number with ridiculous precision as pip in tox
+# does not reinstall the same version.
+import datetime
+VERSION = "git-" + datetime.datetime.now().isoformat()
+
+
+options = dict(
+    name="pystil",
+    version=VERSION,
+    description="An elegant site web traffic analyzer",
+    long_description=__doc__,
+    author="Florian Mounier - Kozea",
+    author_email="florian.mounier@kozea.fr",
+    license="BSD",
+    platforms="Any",
+    packages=find_packages(),
+    install_requires=['pygeoip', 'Multicorn', "flask"],
+    classifiers=[
+        "Development Status :: WIP",
+        "Intended Audience :: Public",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: Linux",
+        "Programming Language :: Python :: 2.7"])
+
+setup(**options)
