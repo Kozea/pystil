ac403cc68eac59c918ce83d48dc1beb98ddd3484
Florian Mounier <florian.mounier@kozea.fr>
2011-08-12 10:59:43 +0200

    Use __file__ for static and templates

--- pystil/__init__.py
+++ pystil/__init__.py
@@ -10,11 +10,17 @@ from logging import getLogger, INFO
 from pystil.log import get_default_handler
 from pystil.routes import register_common_routes
 from pystil.routes.data import register_data_routes
+import pystil
+import os
 
 
 def app():
     """Create Flask app"""
-    app = Flask(__name__)
+    root = os.path.dirname(pystil.__file__)
+    static_folder = os.path.join(root, 'static')
+    template_folder = os.path.join(root, 'templates')
+    app = Flask(__name__,
+                static_folder=static_folder, template_folder=template_folder)
     handler = get_default_handler()
     getLogger('werkzeug').addHandler(handler)
     getLogger('werkzeug').setLevel(INFO)
