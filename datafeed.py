577ae2ab2a4f2357085f1051d2efdef05cf33b3f
Florian Mounier <florian.mounier@kozea.fr>
2011-08-18 16:53:47 +0200

    Add the (in)famous no_ack=True

--- datafeed.py
+++ datafeed.py
@@ -9,8 +9,9 @@ if __name__ == '__main__':
         host='localhost'))
     channel = connection.channel()
     channel.queue_declare(queue='pystil')
+
     def callback(ch, method, properties, body):
         message = pickle.loads(body)
         message.process()
-    channel.basic_consume(callback, queue='pystil')
+    channel.basic_consume(callback, queue='pystil', no_ack=True)
     channel.start_consuming()
