1f37d5945b9f79e3a356d04df2d1c9e4b45b601b
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2011-08-19 16:09:56 +0200

    Daemonize datafeed

--- datafeed.py
+++ datafeed.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python2
+import os
+import sys
 import pika
 import pickle
 from pystil import config
 
 if __name__ == '__main__':
+    pid = os.fork()
+    if pid:
+        try:
+            open("/run/pystil.pid", 'w').write(str(pid))
+        finally:
+            sys.exit()
+
+    sys.stdout = open(os.devnull, "w")
+    sys.stderr = open("/var/log/pystil.err", "w")
+
     config.freeze()
     connection = pika.BlockingConnection(pika.ConnectionParameters(
         host='localhost'))
