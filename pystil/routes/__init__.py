377dc0427999116226bf1eae972548e41c2b3b8f
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2013-02-28 13:40:50 +0100

    Use prefixfree instead of csstyle

--- pystil/routes/__init__.py
+++ pystil/routes/__init__.py
@@ -4,7 +4,6 @@
 # This file is part of pystil, licensed under a 3-clause BSD license.
 
 from flask import render_template, Response
-import csstyle
 import os
 import pystil
 from pystil.db import db, Visit, count, desc
@@ -47,19 +46,3 @@ def register_common_routes(app, route):
     def site(site):
         """Stats per site or all if site = all"""
         return render_template('site.jinja2', site=site)
-
-    @route("/css.css")
-    def css():
-        """Render the css with CSStyle"""
-        style = os.path.join(
-            os.path.dirname(os.path.abspath(pystil.__file__)),
-            'static', 'css.css')
-        with open(style) as style:
-            text = style.read()
-
-        for browser in csstyle.BROWSERS:
-            browser_parser = getattr(csstyle, browser)
-            text += '\n\n/* CSS for %s */\n\n' % browser
-            parser = csstyle.Parser(text=text)
-            text += repr(browser_parser.transform(parser, keep_existant=False))
-        return Response(text, mimetype='text/css')
