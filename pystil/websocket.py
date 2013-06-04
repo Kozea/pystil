d1cccfc67a98855d8783e5687f50472149bbde75
Florian Mounier <florian.mounier@kozea.fr>
2013-06-04 09:26:05 +0200

    Clean close

--- pystil/websocket.py
+++ pystil/websocket.py
@@ -22,7 +22,7 @@ adb = momoko.Pool(dsn=dsn, size=5)
 
 
 @url(r'/last_visits')
-class LastVisitsWebSocket(WebSocketHandler):
+class LastVisitsWebSocket(Hdr, WebSocketHandler):
     waiters = set()
 
     def open(self):
@@ -70,7 +70,8 @@ class QueryWebSocket(Hdr, WebSocketHandler):
         site = self.get_secure_cookie('_pystil_site')
         site = site and site.decode('utf-8').split('|')[0]
         self.query = None
-        self.paused = True
+        self.paused = None
+        self.start = None
         if site is None:
             log.warn('Query websocket open without secure cookie')
             self.close()
@@ -117,16 +118,17 @@ class QueryWebSocket(Hdr, WebSocketHandler):
                 self.stop += 20
                 self.paused = False
                 self.cursor.execute(
-                    'FETCH FORWARD 1 FROM visit_cur;')
+                    'FETCH FORWARD 10 FROM visit_cur;')
         elif command == '/status':
             for i, conn in enumerate(adb._pool):
                 if conn.busy():
                     self.write_message(
                         'INFO|Connection %d is busy: '
-                        'Executing? %s Closed? %d Status? %s' % (
+                        'Executing? %s Closed? %d Status? %s (%d)' % (
                             i, conn.connection.isexecuting(),
                             conn.connection.closed,
-                            conn.connection.get_transaction_status()))
+                            conn.connection.get_transaction_status(),
+                            conn.connection.get_backend_pid()))
                 else:
                     self.write_message('INFO|Connection %d is free' % i)
 
@@ -137,7 +139,7 @@ class QueryWebSocket(Hdr, WebSocketHandler):
                 if self.start:
                     log.info('No connection')
                     self.write_message('BUSY|Server busy, waiting for slot')
-                self.start = False
+                    self.start = False
                 if self.ws_connection:
                     return adb._ioloop.add_callback(partial(
                         self.execute, query, parameters))
@@ -145,7 +147,6 @@ class QueryWebSocket(Hdr, WebSocketHandler):
                 return
             self.start = True
             self.write_message('BEGIN|Searching')
-            self.paused = False
             self.connection = self.momoko_connection.connection
             self.cursor = self.connection.cursor(
                 cursor_factory=psycopg2.extras.NamedTupleCursor)
@@ -165,7 +166,7 @@ class QueryWebSocket(Hdr, WebSocketHandler):
             state = self.connection.poll()
         except psycopg2.extensions.QueryCanceledError:
             log.info('Canceling request %r' % self, exc_info=True)
-            self.cursor.execute('ROLLBACK')
+            self.cursor.execute('ROLLBACK -- CANCEL')
             self.terminated = True
         except (psycopg2.Warning, psycopg2.Error):
             log.exception('Poll error')
@@ -178,17 +179,12 @@ class QueryWebSocket(Hdr, WebSocketHandler):
                     self.momoko_connection.ioloop.remove_handler(
                         self.momoko_connection.fileno)
                     return
-                try:
-                    rows = self.cursor.fetchmany()
-                except:
-                    self.connection.cancel()
-                    self.momoko_connection.ioloop.remove_handler(
-                        self.momoko_connection.fileno)
-                    return
+
+                rows = self.cursor.fetchmany()
 
                 if not rows:
                     self.terminated = True
-                    self.cursor.execute('CLOSE visit_cur; ROLLBACK;')
+                    self.cursor.execute('CLOSE visit_cur; ROLLBACK; -- NOROWS')
                     try:
                         self.write_message('END|Done found %d visit%s' % (
                             self.count, 's' if self.count > 1 else ''))
@@ -204,7 +200,8 @@ class QueryWebSocket(Hdr, WebSocketHandler):
                     except Exception as e:
                         log.warn('During write', exc_info=True)
                         self.terminated = True
-                        self.cursor.execute('CLOSE visit_cur; ROLLBACK;')
+                        self.cursor.execute(
+                            'CLOSE visit_cur; ROLLBACK; -- WSERROR')
                         try:
                             self.write_message(
                                 'END|%s: %s' % (type(e), str(e)))
@@ -213,7 +210,7 @@ class QueryWebSocket(Hdr, WebSocketHandler):
                     else:
                         if self.count < self.stop:
                             self.cursor.execute(
-                                'FETCH FORWARD 1 FROM visit_cur;')
+                                'FETCH FORWARD 10 FROM visit_cur;')
                         else:
                             self.paused = True
                             try:
@@ -233,8 +230,10 @@ class QueryWebSocket(Hdr, WebSocketHandler):
 
     def on_close(self):
         log.info('Closing %r' % self)
-        if not self.start:
+        if (not self.start) or self.terminated:
             return
         if self.paused:
-            self.cursor.execute('CLOSE visit_cur; ROLLBACK;')
-        self.connection.cancel()
+            self.terminated = True
+            self.cursor.execute('CLOSE visit_cur; ROLLBACK; -- ONCLOSE')
+        else:
+            self.connection.cancel()
